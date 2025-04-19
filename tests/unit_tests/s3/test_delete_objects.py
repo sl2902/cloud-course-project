4e2ff6d2c156238e7ed00f6df214edf9bf2ef16a
sl2902 <laxmsun@hotmail.com>
2025-04-19 14:43:32 +0530

    Added tests for files_api

--- tests/unit_tests/s3/test_delete_objects.py
+++ tests/unit_tests/s3/test_delete_objects.py
@@ -1,34 +1,25 @@
 """Test cases for `s3.delete_objects`."""
 
-from src.files_api.s3.delete_objects import (
-    delete_s3_object,
-)
+import boto3
+
+from src.files_api.s3.delete_objects import delete_s3_object
 from src.files_api.s3.read_objects import object_exists_in_s3
 from src.files_api.s3.write_objects import upload_s3_object
-import boto3
 from tests.consts import TEST_BUCKET_NAME
 
 
 def test_delete_existing_s3_object(mocked_aws: None):
-    s3_client = boto3.client("s3")
+    boto3.client("s3")
     object_key = "file_to_delete.txt"
-    upload_s3_object(
-        TEST_BUCKET_NAME,
-        object_key,
-        b"Test"
-    )
+    upload_s3_object(TEST_BUCKET_NAME, object_key, b"Test")
     delete_s3_object(TEST_BUCKET_NAME, object_key)
     assert object_exists_in_s3(TEST_BUCKET_NAME, object_key) is False
 
 
 def test_delete_nonexistent_s3_object(mocked_aws: None):
-    s3_client = boto3.client("s3")
+    boto3.client("s3")
     object_key = "file_to_delete.txt"
-    upload_s3_object(
-        TEST_BUCKET_NAME,
-        object_key,
-        b"Test"
-    )
+    upload_s3_object(TEST_BUCKET_NAME, object_key, b"Test")
     delete_s3_object(TEST_BUCKET_NAME, object_key)
     delete_s3_object(TEST_BUCKET_NAME, object_key)
     assert object_exists_in_s3(TEST_BUCKET_NAME, object_key) is False
