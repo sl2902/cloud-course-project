68814623a2e513699cded66d6272df6e4136421f
sl2902 <laxmsun@hotmail.com>
2025-04-24 23:07:02 +0530

    Add function to delete objects and bucket

---
+++ tests/utils.py
@@ -0,0 +1,7 @@
+import boto3
+
+def delete_s3_bucket(bucket_name: str) -> None:
+    s3_delete = boto3.resource("s3")
+    bucket = s3_delete.Bucket(bucket_name)
+    bucket.objects.all().delete()
+    bucket.delete()
