04d1a9a83ccd89be38e8cdbfee491a03742b7b60
sl2902 <laxmsun@hotmail.com>
2025-04-28 23:47:07 +0530

    Update the test to reflect parameter change

--- tests/unit_tests/test__routes.py
+++ tests/unit_tests/test__routes.py
@@ -8,10 +8,10 @@ from tests.consts import TEST_BUCKET_NAME
 def test_upload_file(client: TestClient):
     prefix = "some_folder/test.txt"
     content = b"test"
-    content_type = "plain/text"
+    content_type = "text/plain"
     response = client.put(
         f"/v1/files/{prefix}",
-        files={"file": (prefix, content, content_type)},
+        files={"file_content": (prefix, content, content_type)},
     )
 
     assert response.status_code == status.HTTP_201_CREATED
@@ -20,7 +20,7 @@ def test_upload_file(client: TestClient):
     content = b"updated"
     response = client.put(
         f"/v1/files/{prefix}",
-        files={"file": (prefix, content, content_type)},
+        files={"file_content": (prefix, content, content_type)},
     )
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"file_path": prefix, "message": f"Existing file updated at path: /{prefix}"}
@@ -30,7 +30,7 @@ def test_list_files_with_pagination(client: TestClient):
     for i in range(15):
         client.put(
             f"/v1/files/file_{i}",
-            files={"file": (f"file_{i}", b"test", "plain/text")},
+            files={"file_content": (f"file_{i}", b"test", "plain/text")},
         )
     response = client.get("/v1/files?page_size=10")
     assert response.status_code == 200
@@ -40,7 +40,7 @@ def test_list_files_with_pagination(client: TestClient):
 
 
 def test_get_file_metadata(client: TestClient):
-    client.put("/v1/files/file_1", files={"file": ("file_1", b"test", "text/plain")})
+    client.put("/v1/files/file_1", files={"file_content": ("file_1", b"test", "text/plain")})
     assert object_exists_in_s3(TEST_BUCKET_NAME, "file_1")
     response = client.head("/v1/files/file_1")
     assert response.status_code == 200
@@ -51,14 +51,14 @@ def test_get_file_metadata(client: TestClient):
 
 
 def test_get_file(client: TestClient):
-    client.put("/v1/files/file_1", files={"file": ("file_1", b"test", "text/plain")})
+    client.put("/v1/files/file_1", files={"file_content": ("file_1", b"test", "text/plain")})
     response = client.get("/v1/files/file_1")
     assert response.status_code == 200
     assert response.content == b"test"
 
 
 def test_delete_file(client: TestClient):
-    client.put("/v1/files/file_1", files={"file": ("file_1", b"test", "text/plain")})
+    client.put("/v1/files/file_1", files={"file_content": ("file_1", b"test", "text/plain")})
     response = client.delete("/v1/files/file_1")
     assert response.status_code == 204
 
