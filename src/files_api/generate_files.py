e362048487ea2dd55157c4d90ff3bdbef35b8aad
sl2902 <laxmsun@hotmail.com>
2025-05-26 00:53:35 +0530

    Initial commit

---
+++ src/files_api/generate_files.py
@@ -0,0 +1,63 @@
+from typing import (
+    Literal,
+    Tuple,
+    Union,
+)
+
+from openai import AsyncOpenAI
+from openai.types.chat import ChatCompletion
+
+SYSTEM_PROMPT = "You are an autocompletion tool that produces text files given constraints."
+
+async def get_text_chat_completion(prompt: str) -> str:
+    """Generate a text chat completion from a given prompt."""
+    client = AsyncOpenAI()
+
+    response: ChatCompletion = await client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": prompt},
+        ],
+        max_tokens=100,
+        n=1
+    )
+
+    return response.choices[0].message.content or ""
+
+async def generate_image(prompt: str) -> Union[str, None]:
+    """Generate an image from a given prompt."""
+    client = AsyncOpenAI()
+
+    response = await client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024*1024",
+        quality="standard",
+        n=1
+    )
+
+    return response.data[0].url or None
+
+async def generate_text_to_speech(
+        prompt: str,
+        response_format: Literal["mp3", "opus", "aac", "flac", "wav", "pcm"] = "mp3",
+        ) -> Tuple[bytes, str]:
+    """
+    Generate text-to-speech audio from a given prompt.
+
+    Returns the audio content as bytes and the MIME type as a string.
+    """
+    client = AsyncOpenAI()
+
+    response = await client.audio.speech.with_raw_response.create(
+        model="tts-1",
+        voice="echo",
+        input=prompt,
+        response_format=response_format,
+    )
+
+    file_content_bytes: bytes = response.content
+    file_mime_type: str = response.headers.get("Content-Type")
+
+    return file_content_bytes, file_mime_type
